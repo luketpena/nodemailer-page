f4ab16deae8ace3a1255480b890b61e119196f4f
Luke Peña <luketpena@gmail.com>
2019-12-01 14:34:23 -0600

    Can send an email

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 //>> Requires
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
 
 //>> Setup
 const app = express();
@@ -10,8 +12,30 @@ const port = 5000;
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//>> Listens
+//>> Nodemailer
+var transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+        user: 'luketpena@gmail.com',
+        pass: '@zgar0th'
+    }
+});
 
+const mailOptions = {
+  from: 'sender@email.com', // sender address
+  to: 'luketpena@gmail.com', // list of receivers
+  subject: 'Subject of your email', // Subject line
+  html: '<p>Hope nodemailer worked!</p>'// plain text body
+};
+
+transporter.sendMail(mailOptions, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log(info);
+});
+
+//>> Listens
 app.listen(port, ()=>{
   console.log('Hey, listen!',port);
 })
